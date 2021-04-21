19ec49ffc21d1521b44dafa18f0bc6d3d72cfb61
asrlopes <a.junior.srlopes@gmail.com>
2021-04-21 04:27:17 -0400

    add required params and derive

--- lib/inmana/restaurant.ex
+++ lib/inmana/restaurant.ex
@@ -3,6 +3,11 @@ defmodule Inmana.Restaurant do
   import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:email, :name]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
   schema "restaurants" do
     field :email, :string
     field :name, :string
@@ -12,8 +17,8 @@ defmodule Inmana.Restaurant do
 
   def changeset(params) do
     %__MODULE__{}
-    |> cast(params, [:email, :name])
-    |> validate_required([:email, :name])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
     |> validate_length(:name, min: 2)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
